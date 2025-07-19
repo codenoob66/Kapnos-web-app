fb296a7603fc1f45045cba160aab9325268e9b7f
rafael vincent <d749107@gmail.com>
2025-07-19 20:33:16 +0800

    test

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button, Heading, Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={6}>
+      <Heading mb={4}>Hello Vite + Chakra UI</Heading>
+      <Button colorScheme="teal">Click Me</Button>
+    </Box>
+  );
+}
+
+export default App;
