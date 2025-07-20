4e220a9486b290d4cb26298040815f5a3b19ee19
rafael vincent <d749107@gmail.com>
2025-07-20 10:50:18 +0800

    hey

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,31 @@
 import express from "express";
-import dontenv from "dotenv";
+import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
-import path from "path";
 
-dontenv.config();
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5100;
 
+const __dirname = path.resolve();
+
+// Enable CORS
+app.use(
+  cors({
+    origin: "https://kapnos-web-app.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+// API routes
 app.use("/api/products", productRoutes);
 
+// Start server
 app.listen(PORT, () => {
   connectDB();
   console.log("Server started at http://localhost:" + PORT);
