4ac7080a2b4184e73f599752c285ea72b4e6475b
rafael vincent <d749107@gmail.com>
2025-07-20 10:37:55 +0800

    hey

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import CreatePage from "./pages/CreatePage";
 import HomePage from "./pages/HomePage";
 
