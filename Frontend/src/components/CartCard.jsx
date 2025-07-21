fae080f734c56561f61f2c84543c16770c13d9ce
rafael vincent <d749107@gmail.com>
2025-07-22 01:28:57 +0800

    testing the functionality

--- Frontend/src/components/CartCard.jsx
+++ Frontend/src/components/CartCard.jsx
@@ -1,7 +1,34 @@
+import { Container } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Text } from "@chakra-ui/react";
+import { useCartStore } from "../cart/cart";
 
 const CartCard = () => {
-  return <div>this is a cartcomponent</div>;
+  const { cart } = useCartStore();
+  return (
+    <Container>
+      {cart.length === 0 && (
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight="bold"
+          color="gray.500"
+        >
+          Your Cart is empty 😢{" "}
+          <Link to={"/"}>
+            <Text
+              as="span"
+              color="blue.500"
+              _hover={{ textDecoration: "underline" }}
+            >
+              Please add items to your cart
+            </Text>
+          </Link>
+        </Text>
+      )}
+    </Container>
+  );
 };
 
 export default CartCard;
