eed5e5f74c4362e33366fad1b96789e21ed3c81f
rafael vincent <d749107@gmail.com>
2025-07-22 01:52:33 +0800

    testing cart ui

--- Frontend/src/components/CartCard.jsx
+++ Frontend/src/components/CartCard.jsx
@@ -1,7 +1,7 @@
 import { Container, Image, VStack } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Text } from "@chakra-ui/react";
+import { Text, Box } from "@chakra-ui/react";
 import { useCartStore } from "../cart/cart";
 
 const CartCard = () => {
